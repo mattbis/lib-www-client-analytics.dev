d197bed1eb54b75a6bf4eda713b0b0d47400883e
Matthew <863968+mattbis@users.noreply.github.com>
2023-01-13 17:49:57 +0000

    Update proto.dev.js

--- src/proto.dev.js
+++ src/proto.dev.js
@@ -1,3 +1,29 @@
+// todo: scope
+
 /* const runtimeHelpers // dont look for helpers */
-const initNow= Date.now()
-class ViewTimeHelper {}
+const lwwwca_InitDate= Date.now()
+
+/* determine available libraries for viewTimeHelper Resolution */
+function __lwca_getRuntimeJsEnv() {}
+
+/* use modern api / or fallback into one timer for no cpu */
+class _lwca_ViewTimeHelper {
+  /* cannot block must use this instead : with manifest for track type */
+  workers= {}
+  /* for the page, the only central emitters */
+  registry= {}
+  constructor() {
+  }
+  init() {}
+  _LegacyTimer() {}
+  _legacyTimerClearHandle() {}
+  start()
+  stop()
+  focused() {}
+  absent() {}
+  idled() {}
+  /* type= .. 3 */
+  //mixin
+  //get(..){}
+  //export(..){}
+}
